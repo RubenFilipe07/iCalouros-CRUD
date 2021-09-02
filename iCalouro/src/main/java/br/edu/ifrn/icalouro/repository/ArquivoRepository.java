00efac231030b3c098fad577c104c836a7b48d68
Jeremias Saldanha <57194226+thejeremias@users.noreply.github.com>
2021-09-02 20:33:25 -0300

    Delete iCalouro directory

--- iCalouro/src/main/java/br/edu/ifrn/icalouro/repository/ArquivoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifrn.icalouro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.edu.ifrn.icalouro.dominio.Arquivo;
-
-/** Esta interface é o repositório da classe Arquivo.*/
-public interface ArquivoRepository extends JpaRepository<Arquivo, Long>{
-}
